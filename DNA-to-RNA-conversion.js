160942655c5e016e6e3f98f6f8802315ff59a783
Qas1m0m0 <Qasim.17@hotmail.com>
2023-08-01 12:45:11 +0100

    added a new codewars challenge

--- DNA-to-RNA-conversion.js
+++ DNA-to-RNA-conversion.js
@@ -9,7 +9,7 @@
 // "GCAT"  =>  "GCAU"
 // The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 
-//My solution;
+//My solution below;
 
 function DNAtoRNA(dna){
     return dna.replace(/T/g, 'U');
