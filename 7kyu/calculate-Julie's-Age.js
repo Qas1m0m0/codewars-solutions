5a86c6d9adb71cac48cd546b203d3d4ca8d7957b
Qas1m0m0 <Qasim.17@hotmail.com>
2023-10-04 15:48:13 +0100

    codewars solution added

---
+++ 7kyu/calculate-Julie's-Age.js
@@ -0,0 +1,7 @@
+// Julie is x years older than her brother, and she is also y times as old as him.
+// Given x and y calculate Julie's age using the function age(x, y).
+
+//My solution below;
+
+function age(x, y){return x*y/(y-1)}
+
